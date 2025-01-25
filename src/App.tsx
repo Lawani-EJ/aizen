e8210c36e76e12cf0c7d3ed6c14bb104277075af
Lawani-EJ <elyonjohn60@gmail.com>
2025-01-25 16:59:02 +0100

    feat: Implement the chat functionallity using stream chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import { StreamChat, type User, type Channel as StreamChannel } from 'stream-chat';
+import { Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import 'stream-chat-react/dist/css/v2/index.css';
+import './layout.css';
+
+const apiKey = '5qpb6gfdmpu8';
+const userId = 'john609';
+const userName = 'Chat_app12';
+const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiam9objYwOSJ9.KW4rl6qxO68WlxutxLlToao86wspyN9qsmcb-L57yds';
+
+const user: User = {
+  id: userId,
+  name: userName,
+  image: `https://getstream.io/random_png/?name=${userName}`,
+};
+
+const App = () => {
+  const [channel, setChannel] = useState<StreamChannel | null>(null);
+  const [client, setClient] = useState<StreamChat | null>(null);
+
+  useEffect(() => {
+    const initClient = async () => {
+      const chatClient = StreamChat.getInstance(apiKey);
+  
+      try {
+        await chatClient.connectUser(user, userToken);
+  
+        const chatChannel = chatClient.channel('messaging', 'custom_channel_id', {
+          members: [userId],
+        });
+  
+        await chatChannel.update({
+          name: "Welcome to Aizen's Chat Room",
+          image: 'https://e7.pngegg.com/pngimages/744/949/png-clipart-sōsuke-aizen-rangiku-matsumoto-renji-abarai-byakuya-kuchiki-ichigo-kurosaki-ichigo-kurosaki-black-hair-hand-thumbnail.png',
+        });
+  
+        await chatChannel.watch();
+  
+        setClient(chatClient);
+        setChannel(chatChannel);
+      } catch (error) {
+        console.error('Error initializing Stream Chat client:', error);
+      }
+    };
+  
+    initClient();
+  
+    return () => client?.disconnectUser();
+  }, []);
+  
+
+  if (!client) return <div>Setting up client & connection...</div>;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Chat client={client}>
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+};
 
-export default App
+export default App;
